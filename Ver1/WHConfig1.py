f8ddeab3a0ed60800fabc3c5b7e1d2540933a012
Jacob Wu <jacob.wu@my.bdsc.school.nz>
2026-05-27 09:40:33 +1200

    Added workouts to the unit list and created a time to unit converter dictionary.

--- Ver1/WHConfig1.py
+++ Ver1/WHConfig1.py
@@ -3,17 +3,27 @@ This Section is where the miscellaneous things happen like defining the rate of 
 """
 # Menus
 menu = ["Workout", "Previous Workouts", "Contact Us", "Log Out"]
-workout_menu = ["WALK", "RUN", "BIKE", "BASKETBALL", "SKATING", "BADMINTON"]
+workout_menu = ["WALK", "RUN", "BIKE", "BASKETBALL", "SKATING", "GYM", "BADMINTON"]
 # Print Menus
 def print_menu(menus):
     line = "-" * len(" | ".join(menus))
     print(line)
     print(" | ".join(menus))
     print(line)
+# Which workout fits into which unit of measurement
+kilometres = ["WALK", "RUN", "BIKE"]
+kilojoules = ["HIGH JUMP", "GYM", "BADMINTON", "BASKETBALL", "SKATING"]
 
-kilometres = ["WALK", "RUN", "BIKE", "BASKETBALL"]
-kilojoules = ["HIGH JUMP", "GYM"]
-
+# The convert rate for time * multiplier for each workout
+converters = {"WALK": 1.39,
+              "RUN": 2.78,
+              "BIKE": 6.94,
+              "BASKETBALL": 0.75,
+              "SKATING": 20,
+              "HIGH JUMP": 0.5,
+              "GYM": 31,
+              "BADMINTON": 3
+              }
 
 if __name__ == "__main__":
     print_menu(workout_menu)
