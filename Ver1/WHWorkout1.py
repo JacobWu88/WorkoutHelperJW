8df25f5b54a7338d8d481691217bd998af7e1a23
Jacob Wu <jacob.wu@my.bdsc.school.nz>
2026-06-09 12:59:12 +1200

    Removed the constant looping of the same function eg reading a file and writing.

--- Ver1/WHWorkout1.py
+++ Ver1/WHWorkout1.py
@@ -11,12 +11,12 @@ def workout(user_id):
         converted = timing * multiplier
         return converted
     while True:
-        workout_option = input("What workout do you want to do? (or go 'back') ").upper()
-        workout_name = cf.number_to_workout.get(workout_option)
-        print(workout_name)
+        workout_name = input("What workout do you want to do? (or go 'back') ").upper()
         if workout_name == "BACK":
             break
-        elif workout_name in cf.workout_menu:
+        if workout_name in cf.number_to_workout.keys():
+            workout_name = cf.number_to_workout.get(workout_name)
+        if workout_name in cf.workout_menu:
             # Define the unit of the workout
             if workout_name in cf.kilometres:
              special = "m"
@@ -42,33 +42,23 @@ def workout(user_id):
             print("GO!")
             # Start time is the current time
             start_time = time.time()
-            choice = "go"
-            lapped = False
-            while choice != "stop":
+
+            # Lap data is a list of lap times
+            lap_data = []
+            while True:
                 # Lap
                 choice = input("Enter 'L' to lap or Press Enter to stop the workout. ").lower()
                 if choice == "l":
                     # Calculate the lap time
                     lap_time = round(time.time() - start_time, 2)
-                    print(f"Lap time: {lap_time} seconds")
                     special_value = round(convert(lap_time), 2)
-                    with open("lap.txt", "a") as f:
-                        # Write the workout ID, Name, value, unit, and laptime to the file
-                        f.write(f"\n{workout_id} {user_id} {workout_name} {special_value}{special} {lap_time}s")
-                        lapped = True
-                        while choice != "stop":
-                            choice = input("Enter 'L' to lap or Press Enter to stop the workout. ").lower()
-                            if choice == "l":
-                                lap_time = round(time.time() - start_time, 2)
-                                print(f"Lap time: {lap_time} seconds")
-                                special_value = round(convert(lap_time), 2)
-                                f.write(f" {special_value}{special} {lap_time}s")
-                            elif choice == "":
-                                print("Stopping Workout.")
-                                choice = "stop"
+                    print(f"Lap time: {lap_time} seconds")
+                    # Add the lap time to the lap_data list
+                    lap_data.append(f"{special_value}{special} {lap_time}s")
+                # End workout
                 elif choice == "":
                     print("Stopping Workout.")
-                    choice = "stop"
+                    break
                 else:
                     print("Invalid")
             #  End time is the time the user stopped the workout
@@ -79,10 +69,16 @@ def workout(user_id):
             print(f"Your workout took {total_time} seconds, {special_value}{special}")
             # Write the workout to a file
             with open("workout_history.txt", "a") as file:
-                file.write(f"{user_id} {workout_id} {workout_name} {special_value}{special} {time.strftime("%d/%m/%Y")} {total_time}    \n")
-            if not lapped:
-                with open("lap.txt", "a") as file:
-                    file.write(f"\n{workout_id} {user_id} {workout_name} {special_value}{special} {total_time}s")
+                # Write the workout to the workout file
+                file.write(f"{user_id} {workout_id} {workout_name} {special_value}{special} {time.strftime('%d/%m/%Y')} {total_time}    \n")
+
+                with open("lap.txt", "a") as f:
+                    # If there are laps, write them to the lap file
+                    if lap_data:
+                        f.write(f"\n{workout_id} {user_id} {workout_name} {' '.join(lap_data)}")
+                    # If there are no laps, write the total time to the lap file
+                    else:
+                        f.write(f"\n{workout_id} {user_id} {workout_name} {special_value}{special} {total_time}s")
         else:
             print("Invalid Workout")
 if __name__ == "__main__":
