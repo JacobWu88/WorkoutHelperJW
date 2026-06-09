17cf30deaa3f2bb677bf60dbb6120572546d92c1
Jacob Wu <jacob.wu@my.bdsc.school.nz>
2026-06-10 09:44:57 +1200

    reverted deleting files

---
+++ Assessment scripts/WHContactUs.py
@@ -0,0 +1,8 @@
+def contact_us():
+    print("Contact us at: ")
+    print("Email: jacob.wu@my.bdsc.school.nz")
+    print("Phone: 028 947 2643")
+    print("Address: 575 Chapel Road, East Tamaki, Auckland 1022, New Zealand")
+    return
+if __name__ == "__main__":
+    contact_us()
